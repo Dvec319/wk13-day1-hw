e0d45dd8eb8df82ec20a5598a43ea8314ff10fb2
mdelvecchio <mdelvecchio3119@gmail.com>
2023-07-19 21:33:31 -0400

    Messing around with the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,24 @@
 function Footer  (props) {
 
 	//The Components Returned JSX
-	return <div className='Footer'> This is the Footer</div>;
+	return (
+		<div className='Footer'>
+			<nav className='navbar'>
+				<h1 className='nav-link'>
+					<a className='nav-link' href='https://github.com/Dvec319'>
+						My Github!
+					</a>
+				</h1>
+				<h1 className='nav-link'>
+					<a
+						className='nav-link'
+						href='https://www.linkedin.com/in/michaelcdelvecchio/'>
+						My Linkedin!
+					</a>
+				</h1>
+			</nav>
+		</div>
+	);
 };
 
 // export the component
